ca6f97491ef008fd43f5c5e30b86c19ba00fb517
Boy Maas <boy.maas@gmail.com>
2018-04-24 19:15:53 +0200

    Initial readme and script skeleton

---
+++ script.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# When not available checkout the EOS source
+
+# Apply all the patches in the patch.d directory
+
+# Perform the build
+
+# Assuming all went ok
+
+# Spit out a env file so we have a "sandboxed" version
