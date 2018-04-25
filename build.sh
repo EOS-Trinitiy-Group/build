0135da700e85e4abcea9e2397e934d5f36b507f3
Boy Maas <boy.maas@gmail.com>
2018-04-25 23:55:34 +0200

    ensure_context and ceremony

--- build.sh
+++ build.sh
@@ -1,8 +1,17 @@
+#!/bin/bash
+
+ensure_context() {
+    set -o errexit
+    cd "$(dirname "$0")"
+}
+
 build() {
     (cd eos && ./eosio_build.sh 2>&1 >../build.log)
 }
 
-echo "Building"
+echo "Building .."
+
+ensure_context
 
 if build ; then
     echo "Build completed successfully"
